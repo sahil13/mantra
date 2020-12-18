c13f928e26be7adad4736cd41c37be05cab8655e
sahilarora13 <sahil.arora02@nagarro.com>
2020-12-18 15:10:14 +0530

    filter added in vehicle registration mainpage

--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle',
@@ -9,21 +10,28 @@ export class VehicleComponent implements OnInit {
   @ViewChild('test') test: ElementRef;
   viewpage;
   viewData;
+  filteredData;
   constructor(private elem: ElementRef) {}
 
-  customerVehicleData = [
+  // tslint:disable-next-line:ban-types
+  customerVehicleData: Array<Object> = [
     { customer_name: 'Sahil Arora', vrn: 'HR26AG1111' },
     { customer_name: 'Gunjan', vrn: 'HR26AG0100' },
   ];
 
-  ngOnInit(): void {}
-
+  ngOnInit(): void {
+    this.filteredData = this.customerVehicleData;
+  }
 
   viewRecord(i) {
     this.viewpage = 1;
     this.viewData = this.customerVehicleData[i];
   }
-  deleteRecord(i){
-
+  deleteRecord(i) {}
+  filterData(value) {
+    // this.customerVehicleData.filter(this.filterValues);
+    this.filteredData = this.customerVehicleData.filter((e) => {
+      return e.customer_name === value;
+    });
   }
 }
